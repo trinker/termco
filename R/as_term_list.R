f7b6f1b1dc6b7e77b2d86d93793619ac86fb639a
Tyler <tyler.rinker@gmail.com>
2017-04-14 22:29:32 -0400

    added dictionary quanteda to as_term_list

--- R/as_term_list.R
+++ R/as_term_list.R
@@ -1,9 +1,11 @@
-#' Convert Vector to Named List
+#' Coerce to Named List
 #'
-#' Convenience function to convert a vector of terms into a named list.  Names
-#' are the same as the terms.
+#' Convenience function to convert a data forms of terms into a named list.  For
+#' vectors, names are the same as the terms.
 #'
-#' @param x A vector of strings.
+#' @param x A vector of strings or a \pkg{quanteda} \code{dictionary}.
+#' @param add.boundary logical.  If \code{TRUE} a word boundary is place at the
+#' beginning and end of the strings.
 #' @param \ldots ignored.
 #' @return Returns a named list.
 #' @export
@@ -25,10 +27,43 @@
 #'
 #' presidential_debates_2012 %>%
 #'     with(term_count(dialogue, person, bigrams))
+#'
+#' ## dictionary from quanteda
+#' require(quanteda)
+#' mfdict <- dictionary(
+#'     file = "http://ow.ly/VMRkL",
+#'     format = "LIWC"
+#' )
+#'
+#' as_term_list(mfdict, TRUE)
 #' }
-as_term_list <- function(x, ...){
+as_term_list <- function(x, add.boundary = FALSE, ...){
+    UseMethod('as_term_list')
+}
 
-    stopifnot(is.atomic(x))
+#' @export
+#' @method as_term_list character
+as_term_list.character <- function(x, add.boundary = FALSE, ...){
+    if (isTRUE(add.boundary)){
+        x <- paste0('\\b', x, '\\b')
+    }
     stats::setNames(as.list(x), gsub('\\s+', '_', x))
 }
+
+#' @export
+#' @method as_term_list dictionary
+as_term_list.dictionary <- function(x, add.boundary = FALSE, ...){
+
+    stats::setNames(lapply(names(x), function(y){
+        out <- x[[y]]
+        if (isTRUE(add.boundary)){
+            out <- gsub("^|(?<!\\*)$", "\\\\b", out, perl = TRUE)
+        }
+        gsub("\\*", '[A-Za-z0-9-]*', out)
+    }), names(x))
+
+}
+
+## http://www.moralfoundations.org/sites/default/files/files/downloads/moral%20foundations%20dictionary.dic
+
 
